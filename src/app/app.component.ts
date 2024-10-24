74a325ad0d1bcefd42229bdf79c57a0cdd9649a3
Benedict <benedictumeozor@gmail.com>
2024-10-24 20:50:29 +0100

    sorted out flash of unstyled content and app theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(themeService: ThemeService) {
+    const theme = localStorage.getItem('theme') as 'light' | 'dark';
+
+    themeService.setTheme(theme);
+  }
+}
